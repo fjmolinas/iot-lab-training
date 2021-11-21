644b53b24b8cdbee0eeed6c3623570b5104a5dbe
Francisco Molina <femolina@uc.cl>
2021-11-21 21:23:20 +0100

    riot/polytech/chirpstack-sensors: initial commit

---
+++ riot/polytech/chirpstack-sensors/main.c
@@ -0,0 +1,30 @@
+#include <string.h>
+
+#include "board.h"
+#include "xtimer.h"
+
+/* Add necessary include here */
+
+/* Declare globally the sensor device descriptor */
+
+/* Device and application informations required for OTAA activation */
+
+int main(void)
+{
+    /* configure the device parameters */
+
+    /* change datarate to DR5 (SF7/BW125kHz) */
+
+    /* start the OTAA join procedure */
+
+    while (1) {
+        char *message = "This is RIOT!";
+
+        /* send the message here */
+
+        /* wait 20 seconds between each message */
+        xtimer_sleep(20);
+    }
+
+    return 0; /* should never be reached */
+}
